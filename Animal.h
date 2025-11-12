487dc6be0909ee1f955a1ae646706c264fbfdd1e
alisherif463-alt <alisherif463@gmail.com>
2025-11-12 15:21:31 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -7,5 +7,25 @@ using namespace std;
 //define animal class here 
 
 
+#include<iostream>
+using namespace std;
 
+class animal
+{
+    private:
+    string name;
+    int age;
+    bool ishungry;
+    public:
+    animal(string n,int a,bool hun);
+    animal();
+    ~animal();
+    void display();
+    void feed();
+   
+    
+    
+    
+    
+};
 #endif
