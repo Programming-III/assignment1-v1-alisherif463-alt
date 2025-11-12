9f10d80541525c6e0c6f9203fc0eb4e203cc4330
alisherif463-alt <alisherif463@gmail.com>
2025-11-12 15:24:46 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -7,7 +7,21 @@
 
 
 
-
+#include<iostream>
+using namespace std;
+class reptile:public animal
+{
+    private:
+    bool isvenomous;
+    public:
+    reptile(bool iv,string n,int a,bool hun);
+    reptile();
+    ~reptile();
+    
+    
+    
+    
+};
 
 
 
