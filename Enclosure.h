9521ba0dd2c4e8c7e1f4f43fc231bea43170cf48
alisherif463-alt <alisherif463@gmail.com>
2025-11-12 15:23:45 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -5,7 +5,24 @@
 
 //define enclosure class here 
 
-
+#include<iostream>
+using namespace std;
+class enclosure
+{
+    private:
+    animal*enclos;
+    int capacity;
+    int count;
+    public:
+    enclosure(int cap,int c);
+    enclosure();
+    ~enclosure();
+    void addanimal(animal*a);
+    void displayanimal();
+    
+    
+    
+};
 
 
 
